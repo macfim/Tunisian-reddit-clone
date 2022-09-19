47bc1f01302472218f8cb44491de63d79c074a6c
J.Boughattas <boughattasjassem@outlook.com>
2022-09-19 19:23:45 +0100

    seperate load more button

---
+++ src/components/Home/LoadMoreButton.js
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+import { useSelector, useDispatch } from "react-redux";
+
+import { loadMore } from "../../Slices/postsSlice";
+
+const LoadMoreButton = () => {
+  const dispatch = useDispatch();
+  const loadMoreStatus = useSelector((state) => state.posts.loadMoreStatus);
+  const loadMoreError = useSelector((state) => state.posts.loadMoreError);
+
+  const handleClick = () => dispatch(loadMore());
+
+  const ButtonWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const Button = styled.button`
+    width: 100%;
+    padding-block: .8rem;
+    font-size: 1rem;
+    background: white;
+    border: 1px solid rgba(0,0,0,.2);
+    cursor: pointer;
+
+    :hover {
+      box-shadow: 0 0 10px rgba(0,0,0,.2);
+    }
+  `;
+
+  const ErrorMessage = styled.div`
+    padding-block: 1rem;
+    color: red;
+    font-weight: 400;
+    font-size: 1.1rem;
+    text-transform: uppercase;
+  `;
+
+  return (
+    <ButtonWrapper>
+      <Button onClick={handleClick}>
+        {loadMoreStatus === "loading" ? 'Loading' : 'LoadMore'}
+      </Button>
+      <ErrorMessage>
+        {loadMoreError ? loadMoreError : null}
+      </ErrorMessage>
+    </ButtonWrapper>
+  );
+};
+
+export default LoadMoreButton;
